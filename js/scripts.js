c140216879c5d7a9bf66b1877499c06b7c0b4166
Ryan Ashby <rjapsu160@gmail.com>
2022-01-18 16:26:00 -0800

    BUGFIX add proper li to ul with flavors
    Co-authored-by: Matt Eilar <meilar@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("button").click(function() {
     let favFlavors = ["chocolate", "vanilla", "bacon", "soylent green", "hot dogs", "red velvet"]
     favFlavors.forEach(function(flavor) { 
-     $("#list").append(flavor + " ");
+      $("#list").append("<li>" +flavor + " " + "</li>");
     });
   });
 });
